5df8600fb641a9fda75a7919b5390a7c03c923f5
serge-belkin <cer_fi@yahoo.com>
2022-12-14 12:01:55 +0200

    add 1 task wo styles

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import { Profile } from 'components/01_Profile/Profile';
+import user from 'components/01_Profile/user.json';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+    </App>
+  
+    
   </React.StrictMode>
 );
